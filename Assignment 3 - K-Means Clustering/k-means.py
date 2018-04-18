8b064978c14a8b68d3acd7a5d611860c20823afb
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2018-04-17 23:22:24 -0700

    K-Means graph plotted

--- Assignment 3 - K-Means Clustering/k-means.py
+++ Assignment 3 - K-Means Clustering/k-means.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Kmeans(object):
     def __init__(self, iterations=1000):
@@ -12,10 +13,32 @@ class Kmeans(object):
 
         return X
 
+    def run(self):
+        k_dict = {2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []}
+        for k in k_dict.keys():
+            k_dict[k] = self.kmeans(K=k)
+            print("K = ", k, " F_mu_c: ", k_dict[k])
+        self.plot_learning_curve(k_dict)
+
+        return None
+
+    def plot_learning_curve(self, plot_curve_dict):
+        x, y = zip(*sorted(plot_curve_dict.items()))
+        fig = plt.figure()
+        plt.title('Value of Potential function vs Value of K')
+        plt.plot(x, y, 'r')
+        plt.ylabel('Value of Potential function')
+        plt.xlabel('Value of K')
+        plt.show()
+        fig.savefig('KMeans_graph.png')
+
+        return None
+
     def kmeans(self, K=3):
         m,n = self.X.shape
         centroid_indexes, centroids = self.randomly_select_centroid_indexes(K=K)
         old_centroids = None
+        data_labels = None
         iterations = 0
         while iterations < self.iterations and (not np.array_equal(old_centroids, centroids)):
             iterations += 1
@@ -23,7 +46,22 @@ class Kmeans(object):
             data_labels = self.assign_centroid(centroids)
             centroids = self.recompute_centroid(old_centroids, data_labels)
 
-        return None
+        F_mu_c = self.compute_potential_function(centroids=centroids, data_labels=data_labels)
+
+        return F_mu_c
+
+    def compute_potential_function(self, centroids, data_labels):
+        F_mu_c = 0
+        for i in range(centroids.shape[0]):
+            indices_belonging_to_i = np.where(data_labels == i)
+            data_belonging_to_i = self.X[indices_belonging_to_i]
+            u = centroids[[i], :]
+            squared = np.square(u - data_belonging_to_i)
+            summed = np.sum(squared, axis=1)
+            sqrted = np.sqrt(summed)
+            F_mu_c += np.sum(sqrted)
+
+        return F_mu_c
 
     def recompute_centroid(self, old_centroids, data_labels):
         new_centroids = np.zeros(shape=old_centroids.shape)
@@ -67,4 +105,4 @@ class Kmeans(object):
 
 if __name__ == "__main__":
     obj = Kmeans(iterations=1000)
-    obj.kmeans(K=3)
+    obj.run()
